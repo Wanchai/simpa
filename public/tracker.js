ff86d70d1b5dbba96c6209fc8d3d0bfb460d3fb9
Thomas Malicet <thomas.malicet@gmail.com>
2026-03-30 22:15:47 +0200

    Update

---
+++ public/tracker.js
@@ -0,0 +1,2 @@
+function call(a){const xhr=new XMLHttpRequest();xhr.open('POST','http://localhost:5173/tracker/');xhr.setRequestHeader('Content-Type','application/json');xhr.send(JSON.stringify({id:a,ref:document.referrer,page:window.location.pathname,}),)}
+function march(a){if(new Date(localStorage.getItem('simpa'+a)).getTime()<new Date().getTime()){call(a);const d=new Date();d.setHours(d.getHours()+12);localStorage.setItem('simpa'+a,d)}}
